42acfc65ddf09cf542f340559884e6861694102a
Lucas Nhimi <lucasnhimi@gmail.com>
2023-02-27 20:40:17 -0300

    Initial code

--- src/app/api/hello/route.ts
+++ src/app/api/hello/route.ts
@@ -1,3 +1,14 @@
+import { NextResponse } from 'next/server';
+
 export async function GET(request: Request) {
-  return new Response('Hello, Next.js!')
+  const { searchParams } = new URL(request.url);
+  const id = searchParams.get('id');
+
+  // const users = await fetch('https://jsonplaceholder.typicode.com/users');
+  // const usersJson = await users.json();
+  return NextResponse.json({ id });
+}
+
+export async function POST(request: Request) {
+  return NextResponse.json({ message: 'Hello, world, POST!' });
 }
